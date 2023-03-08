31314eabb64c1cd0667b9acec028c3a9f2e3abf6
kukon miah <kukon11674@gmail.com>
2023-03-08 18:52:30 -0500

    DeliveryProduct first commit

---
+++ src/test/java/com/kmshoppingapp/testscripts/DeliveryProduct.java
@@ -0,0 +1,31 @@
+package com.kmshoppingapp.testscripts;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.kmshoppingapp.pom.pages.EveningDress;
+import com.kmshoppingapp.pom.pages.Home;
+import com.kmshoppingapp.pom.pages.Login;
+
+import genericlibrarycom.shoppingapp.Baseclass;
+import genericlibrarycom.shoppingapp.Propertyfile;
+import genericlibrarycom.shoppingapp.Utilies;
+
+public class DeliveryProduct extends Baseclass {
+	@Test 
+	public void productdeliver() throws FileNotFoundException, IOException {
+		Login l=new Login(driver);
+		l.loginCredtial(Propertyfile.getpropertyfiledata("email"),Propertyfile.getpropertyfiledata("password"));
+		
+		Home h=new Home(driver);
+		Utilies.mouseHover(driver,h.getDresstab());
+		h.eveningdree();
+		
+		EveningDress e=new EveningDress(driver);
+		e.deliveryproduct();
+		
+	}
+
+}
